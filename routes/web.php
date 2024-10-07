13b97ccaa0ebce36ad98faca8be506c953ef70dc
e.muchasaj <e.muchasaj@gmail.com>
2024-10-07 20:33:25 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/update-user-attributes', [\App\Http\Controllers\UsersController::class, 'updateUserAttributes']);
